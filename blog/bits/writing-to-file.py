6eb595c56cbb97d6dc670ffa8b3242bf7ea80a81
James Thompson <100stacks@users.noreply.github.com>
2016-07-07 16:40:33 -0500

    Create writing-to-file.py

---
+++ blog/bits/writing-to-file.py
@@ -0,0 +1,11 @@
+# Simple example writing to a file
+
+test = 'this is a string\nthat is written to a file`
+
+saveFile = open('test.txt', 'w') # w - (over)write to file, r - read from file, a - append to file
+
+# if text.txt exists, it will delete all content and write our `test` string to it
+saveFile.write(test)
+
+# MAKE SURE TO ALWAYS CLOSE THE FILE
+saveFile.close()
