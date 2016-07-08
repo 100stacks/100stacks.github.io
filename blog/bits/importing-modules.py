d186299b80a68403f9537d68a70bb9ba19290e12
James Thompson <100stacks@users.noreply.github.com>
2016-07-08 12:52:25 -0500

    Update importing-modules.py

--- blog/bits/importing-modules.py
+++ blog/bits/importing-modules.py
@@ -25,3 +25,16 @@ print('standard deviation:', t)
 
 u = vr(sample)
 print('variance:', u)
+
+'''
+Output:
+The following shows a some of the available methods of the
+built-in statistics module.
+Our sample list:
+ [3, 7, 6, 2, 10, 14, 9, 5, 2, 1, 5, 8, 2, 4]
+mean:  5.571428571428571
+median: 5.0
+mode: 2
+standard deviation: 3.7151309266562604
+variance: 13.802197802197803
+'''
