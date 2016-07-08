87ea1a224c22bbb8d90ee0f9dceafe727702d396
James Thompson <100stacks@users.noreply.github.com>
2016-07-08 11:31:39 -0500

    Update statistics.py

--- blog/bits/statistics.py
+++ blog/bits/statistics.py
@@ -15,3 +15,15 @@ print('mean: ', q)
 
 r = statistics.median(sample)
 print('median:', r)
+
+'''
+Output:
+The following shows a some of the available methods of the
+built-in statistics module.
+
+Our sample list:
+ [3, 7, 6, 2, 10, 14, 9, 5, 2, 1, 5, 8, 2, 4]
+mean:  5.571428571428571
+median: 5.0
+'''
+
